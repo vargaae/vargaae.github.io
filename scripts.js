c92a4998c263c0c7b41dfb22b2b01809492996ae
vargaae <58937079+vargaae@users.noreply.github.com>
2021-04-06 20:39:28 +0200

    Refreshed to my work and changed the photos

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
-var counter = 1;
-setInterval(function(){
-  document.getElementById('radio' + counter).checked = true;
-  counter++;
-  if(counter > 4){
-    counter = 1;
-  }
+var counter=1;
+
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4) {
+        counter = 1;
+    }
 }, 5000);
